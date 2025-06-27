8889a9681202bbbed4679faa1b87128f4f58907d
kingdada7 <toluwadada7@gmail.com>
2025-06-27 21:31:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <section className="App">
+        <div className="header-container">
+          <Header />
+        </div>
+
+        <img className="pattern" src="/pattern-lines.svg" alt="" />
+        <img className="circle" src="/pattern-circle.svg" alt="" />
+        <img className="circle-2" src="/pattern-circle.svg" alt="" />
+        <img
+          className="squiggly-top"
+          src="/pattern-squiggly-line-top.svg"
+          alt=""
+        />
+        <img src="/pattern-squiggly-line-bottom-desktop.svg" alt="" />
+      </section>
+    </>
+  );
+}
+
+export default App;
